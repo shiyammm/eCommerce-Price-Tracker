65dde73990debe8bd8f7720eacb4263a19d4bc0e
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-05 23:11:26 +0530

    Modify extractPrice, extractDescription functions and add getHighestPrice, getLowestPrice, getAveragePrice functions.

--- lib/scraperUtils.ts
+++ lib/scraperUtils.ts
@@ -1,14 +1,16 @@
+import { PriceHistoryItem } from '@/types';
+
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
 
     if (priceText) {
-      const cleanPrice = priceText.replace(/[^\d.,]/g, '');
+      const cleanPrice = priceText.replace(/[^\d.]/g, '');
 
       let firstPrice;
 
       if (cleanPrice) {
-        firstPrice = cleanPrice.match(/\d{1,3}(?:,\d{3})*(?:\.\d{2})?/)?.[0]; // Update the regular expression to handle commas
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
       }
 
       return firstPrice || cleanPrice;
@@ -30,16 +32,62 @@ export function extractDiscountPercentage(element: any) {
   return discount ? discount : '';
 }
 
-export function extractDescription(...elements: any) {
-  let description = [];
-  for (let element of elements) {
-    description.push(element);
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    '.a-unordered-list .a-list-item',
+    '.a-expander-content p',
+    // Add more selectors here if needed
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join('\n');
+      return textContent;
+    }
   }
-  return description;
+
+  // If no matching elements were found, return an empty string
+  return '';
 }
 
 export function extractCategory(element: string) {
   const category = element.trim();
 
   return category ? category : '';
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
 }
