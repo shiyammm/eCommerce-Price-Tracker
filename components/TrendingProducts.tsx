a5be4c92ce94f946f794071e7f5c6d08121556a6
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-05 23:17:40 +0530

    Get data from the database and populate it in the trending component

--- components/TrendingProducts.tsx
+++ components/TrendingProducts.tsx
@@ -1,13 +1,48 @@
+import { getAllProducts } from '@/lib/actions';
+import Link from 'next/link';
 import React from 'react';
 
-const TrendingProducts = () => {
-  const products = ['Samsung S24', 'Space Pen', 'LG Smart TV'];
+const TrendingProducts = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <section>
-      {products.map((product) => (
-        <ul key={product}>
-          <li>{product}</li>
-        </ul>
+      {allProducts?.map((product) => (
+        <Link href={`/products/${product._id}`} key={product._id}>
+          <div>
+            {/* Display product information using span and p tags */}
+            <h1>
+              <span>{product.title}</span>
+            </h1>
+            <img src={product.image} alt={product.title} />
+            <p>
+              <span>
+                <strong>Price:</strong> {product.currency}
+                {product.currentPrice}
+              </span>
+              <br />
+              <span>
+                <strong>Original Price:</strong> {product.currency}
+                {product.originalPrice}
+              </span>
+              <br />
+            </p>
+            <p>
+              <span>
+                <strong>Category:</strong> {product.category}
+              </span>
+              <br />
+              <span>
+                <strong>Reviews Count:</strong> {product.reviewsCount}
+              </span>
+              <br />
+              <span>
+                <strong>Out of Stock:</strong>{' '}
+                {product.isOutOfStock ? 'Yes' : 'No'}
+              </span>
+            </p>
+          </div>
+        </Link>
       ))}
     </section>
   );
